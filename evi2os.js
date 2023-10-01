3e66d7032bb421189e12d79393e2e353dfb90e62
Alepst <126186583+Alepst@users.noreply.github.com>
2023-09-30 23:46:25 -0600

    Add files via upload

---
+++ evi2os.js
@@ -0,0 +1,23 @@
+function ardillas (op){
+        switch (op){
+            case 'ardil1':
+                document.body.style.background = "#f3f3f3 url('ardilla1.jpg') no-repeat right top";
+                break;
+            case 'ardill2':
+                document.body.style.background = "#f3f3f3 url('ardilla2.jpg') no-repeat right top";
+                break;
+            case 'ardill3':
+                document.body.style.background = "#f3f3f3 url('ardilla3.jpg') no-repeat right top";
+                break
+            case 'ardill4':
+                document.body.style.background = "#f3f3f3 url('ardilla4.jpg') no-repeat right top";
+                break
+            case 'ardill5':
+                document.body.style.background = "#f3f3f3 url('ardilla5.png') no-repeat right top";
+                break
+            case 'ardill6':
+                document.body.style.background = "#f3f3f3 url('ardilla6.jpg') no-repeat right top";
+                break
+            }
+        }
+
